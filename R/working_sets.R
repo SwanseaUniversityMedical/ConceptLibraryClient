b990c7febb01de8ba3ce3caf8e3f728be6df15de
Etienne Badoche <etienne.badoche@outlook.com>
2021-11-11 16:20:38 +0000

    Refactor working sets functions for consistency.

--- R/working_sets.R
+++ R/working_sets.R
@@ -2,12 +2,12 @@
 #'
 #' Lists all available working sets for the user. This cannot be used with the public API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param search Search by part of working set name (do not put wild characters here)
-#' @param tag_ids Specify vector of tags ids (get tags from get_tags())
+#' @param tag_ids Specify vector of tags ids (get tags from get_tags()). NOT CURRENTLY WORKING.
 #' @param show_only_my_workingsets Only show working sets owned by me. Default is FALSE.
 #' @param show_deleted_workingsets Also show deleted working sets. Default is FALSE.
-#' @param brand Only show working sets with specified brand.
+#' @param brand Only show working sets with specified brand. NOT CURRENTLY WORKING.
 #' @param author Search by part of the author name.
 #' @param owner_username Search by full username of the owner.
 #' @param do_not_show_versions Do not show working sets versions. Default is FALSE (versions are shown).
@@ -16,13 +16,14 @@
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_sets(api_client)
 #' get_working_sets(
 #'   search = 'diab',
-#'   tag_ids = c(11,4),
+#'   tag_ids = c(11,4),                 # Excluded at the moment
 #'   show_only_my_workingsets = TRUE,
 #'   show_deleted_workingsets = TRUE,
-#'   brand = 'ADP',
+#'   brand = 'ADP',                     # Excluded at the moment
 #'   author = 'moh',
 #'   owner_username = 'a.john',
 #'   do_not_show_versions = TRUE)
@@ -30,22 +31,21 @@
 get_working_sets <- function(
   api_client,
   search = NA,
-  tag_ids = NA,
+#  tag_ids = NA,
   show_only_my_workingsets = FALSE,
   show_deleted_workingsets = FALSE,
-  brand = NA,
+#  brand = NA,
   author = NA,
   owner_username = NA,
   do_not_show_versions = FALSE
 ) {
-
   # Create list of named query parameters
   query_params = list(
     search = search,
-    tag_ids = tag_ids,
+  #  tag_ids = tag_ids,
     show_only_my_workingsets = show_only_my_workingsets,
     show_deleted_workingsets = show_deleted_workingsets,
-    brand = brand,
+  #  brand = brand,
     author = author,
     owner_username = owner_username,
     do_not_show_versions = do_not_show_versions
@@ -67,19 +67,19 @@ get_working_sets <- function(
 #'
 #' Lists a working set by id for the user. This cannot be used with the public API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #'
 #' @return A dataframe containing the working set.
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_by_id(api_client, '123')
 #'
 get_working_set_by_id <- function(api_client, id) {
-  path = qq('api/v1/workingsets/@{id}/')
-
   # API call
+  path = qq('api/v1/workingsets/@{id}/')
   response = api_client$get(path = path)
   check_HTTP_response(response)
 
@@ -93,7 +93,7 @@ get_working_set_by_id <- function(api_client, id) {
 #'
 #' Lists the working set detail of the latest version. This cannot be used with the public API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #' @param do_not_show_codes Do not show working set codes. Default is FALSE (codes are shown).
 #'
@@ -101,15 +101,15 @@ get_working_set_by_id <- function(api_client, id) {
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_detail(api_client, '123')
 #' get_working_set_detail(api_client, '123', do_not_show_codes = TRUE)
 #'
 get_working_set_detail <- function(api_client, id, do_not_show_codes = FALSE) {
-  path = qq('api/v1/workingsets/@{id}/detail/')
-
   query_list = clean_query_list(list(do_not_show_codes = do_not_show_codes))
 
   # API call
+  path = qq('api/v1/workingsets/@{id}/detail/')
   response = api_client$get(path = path, query = query_list)
   check_HTTP_response(response)
 
@@ -123,7 +123,7 @@ get_working_set_detail <- function(api_client, id, do_not_show_codes = FALSE) {
 #'
 #' Lists the phenotype detail of the specified version. This cannot be used with the public API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #' @param version_id The working set version's id.
 #' @param do_not_show_codes Do not show working set codes. Default is FALSE (codes are shown).
@@ -132,15 +132,15 @@ get_working_set_detail <- function(api_client, id, do_not_show_codes = FALSE) {
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_detail_by_version(api_client, '123', '456')
 #' get_working_set_detail_by_version(api_client, '123', '456', do_not_show_codes = TRUE)
 #'
 get_working_set_detail_by_version <- function(api_client, id, version_id, do_not_show_codes = FALSE) {
-  path = qq('api/v1/workingsets/@{id}/version/@{version_id}/detail/')
-
   query_list = clean_query_list(list(do_not_show_codes = do_not_show_codes))
 
   # API call
+  path = qq('api/v1/workingsets/@{id}/version/@{version_id}/detail/')
   response = api_client$get(path = path, query = query_list)
   check_HTTP_response(response)
 
@@ -155,19 +155,19 @@ get_working_set_detail_by_version <- function(api_client, id, version_id, do_not
 #' Exports the code list of the latest version of a working set for the user. This cannot be used with the public
 #' API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #'
 #' @return A dataframe containing the working set's code list.
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_code_list(api_client, '123')
 #'
 get_working_set_code_list <- function(api_client, id) {
-  path = qq('api/v1/workingsets/@{id}/export/codes/')
-
   # API call
+  path = qq('api/v1/workingsets/@{id}/export/codes/')
   response = api_client$get(path = path)
   check_HTTP_response(response)
 
@@ -182,7 +182,7 @@ get_working_set_code_list <- function(api_client, id) {
 #' Exports the code list of a specific version of a working set for the user. This cannot be used with the public
 #' API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #' @param version_id The working set version's id.
 #'
@@ -190,12 +190,12 @@ get_working_set_code_list <- function(api_client, id) {
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_code_list_by_version(api_client, '123', '456')
 #'
 get_working_set_code_list_by_version <- function(api_client, id, version_id) {
-  path = qq('api/v1/workingsets/@{id}/version/@{version_id}/export/codes/')
-
   # API call
+  path = qq('api/v1/workingsets/@{id}/version/@{version_id}/export/codes/')
   response = api_client$get(path = path)
   check_HTTP_response(response)
 
@@ -209,19 +209,19 @@ get_working_set_code_list_by_version <- function(api_client, id, version_id) {
 #'
 #' Lists all the versions of the working set for the user. This cannot be used with the public API.
 #'
-#' @param api_client The HttpClient returned by the connectToAPI function.
+#' @param api_client The HttpClient returned by the connectToAPI function. This must be an authenticated connection.
 #' @param id The working set's id.
 #'
 #' @return A dataframe containing the working set's versions.
 #' @export
 #'
 #' @examples
+#' api_client = connect_to_API(public = FALSE)
 #' get_working_set_versions(api_client, '123')
 #'
 get_working_set_versions <- function(api_client, id) {
-  path = qq('api/v1/workingsets/@{id}/get-versions/')
-
   # API call
+  path = qq('api/v1/workingsets/@{id}/get-versions/')
   response = api_client$get(path = path)
   check_HTTP_response(response)
 
